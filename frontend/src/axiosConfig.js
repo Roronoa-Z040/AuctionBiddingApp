f839ac017604298c4316184a400552ddf4839999
Roronoa-Z040 <Roronoa-Z040@users.noreply.github.com>
2025-10-09 13:25:52 +1000

    public ip change

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,5 +1,5 @@
 
 import axios from 'axios';
-const baseURL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5001';
+const baseURL = process.env.REACT_APP_API_URL || 'http://13.211.211.43:5001';
 const instance = axios.create({ baseURL });
 export default instance;
